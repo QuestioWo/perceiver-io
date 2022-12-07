12412cbb43b600bb48eb660e4781929d03dfc468
Jim Carty <cartyjim1@gmail.com>
2022-12-07 11:24:08 +0000

    Updating preprocessing to include coregistration

--- perceiver/data/segmentation/common.py
+++ perceiver/data/segmentation/common.py
@@ -1,4 +1,7 @@
 import torch
+import SimpleITK as sitk
+import skimage
+import sys
 
 
 class SegmentationPreprocessor:
@@ -19,6 +22,107 @@ def lift_transform(transform):
         return sample
 
     return apply
+
+
+def _command_iteration(method):
+	# if method.GetOptimizerIteration() == 0:
+	# 	print(f"\tLevel: {method.GetCurrentLevel()}")
+	# 	print(f"\tScales: {method.GetOptimizerScales()}")
+	# print(f"#{method.GetOptimizerIteration()}")
+	# print(f"\tMetric Value: {method.GetMetricValue():10.5f}")
+	# print(f"\tLearningRate: {method.GetOptimizerLearningRate():10.5f}")
+	# if method.GetOptimizerConvergenceValue() != sys.float_info.max:
+	# 	print(
+	# 		"\tConvergence Value: "
+	# 		+ f"{method.GetOptimizerConvergenceValue():.5e}"
+	# 	)
+	pass
+
+
+def _command_multiresolution_iteration(method):
+	# print(f"\tStop Condition: {method.GetOptimizerStopConditionDescription()}")
+	# print("============= Resolution Change =============")
+	pass
+
+
+def coregister_scan(np_moving, np_moving_label, np_fixed) :
+	fixed = sitk.GetImageFromArray(np_fixed)
+	moving = sitk.GetImageFromArray(np_moving)
+	moving_label = sitk.GetImageFromArray(np_moving_label)
+
+	scale_factor = (max(np_fixed.shape[0], np_fixed.shape[1]) / min(np_moving.shape[1], np_moving.shape[1]))
+	scale_factor_width = int(scale_factor * moving.GetWidth())
+	scale_factor_height = int(scale_factor * moving.GetHeight())
+	scale_factor_depth = int(scale_factor * moving.GetDepth())
+
+	# The spatial definition of the images we want to use in a deep learning framework (smaller than the original).
+	new_size = (scale_factor_width, scale_factor_height, scale_factor_depth)
+	reference_image = sitk.Image(new_size, moving.GetPixelIDValue())
+	reference_image.SetOrigin(moving.GetOrigin())
+	reference_image.SetDirection(moving.GetDirection())
+	reference_image.SetSpacing(
+		[
+			sz * spc / nsz
+			for nsz, sz, spc in zip(new_size, moving.GetSize(), moving.GetSpacing())
+		]
+	)
+
+	moving = sitk.Resample(moving, reference_image)
+	moving_label = sitk.Resample(moving_label, reference_image)
+
+	initialTx = sitk.CenteredTransformInitializer(
+		fixed, moving, sitk.AffineTransform(fixed.GetDimension())
+	)
+
+	R = sitk.ImageRegistrationMethod()
+
+	R.SetShrinkFactorsPerLevel([1, 1, 1])
+	R.SetSmoothingSigmasPerLevel([2, 1, 1])
+
+	R.SetMetricAsJointHistogramMutualInformation(20)
+	R.MetricUseMovingImageGradientFilterOff()
+
+	R.SetOptimizerAsGradientDescent(
+		learningRate=2.0,
+		numberOfIterations=10,
+		estimateLearningRate=R.EachIteration,
+	)
+	R.SetOptimizerScalesFromPhysicalShift()
+
+	R.SetInitialTransform(initialTx)
+
+	R.SetInterpolator(sitk.sitkLinear)
+
+	R.AddCommand(sitk.sitkIterationEvent, lambda: _command_iteration(R))
+	R.AddCommand(
+		sitk.sitkMultiResolutionIterationEvent,
+		lambda: _command_multiresolution_iteration(R),
+	)
+
+	tx = R.Execute(fixed, moving)
+
+	resampler = sitk.ResampleImageFilter()
+	resampler.SetReferenceImage(fixed)
+	resampler.SetInterpolator(sitk.sitkLinear)
+	resampler.SetDefaultPixelValue(-2048)
+	resampler.SetTransform(tx)
+
+	out_moving = resampler.Execute(moving)
+	out_moving_label = resampler.Execute(moving_label)
+
+	new_spacing = [
+		sz * spc / nsz
+		for nsz, sz, spc in zip(fixed.GetSize(), out_moving.GetSize(), out_moving.GetSpacing())
+	]
+
+	out_moving.SetSpacing(new_spacing)
+	out_moving_label.SetSpacing(new_spacing)
+
+	return (sitk.GetArrayFromImage(out_moving), sitk.GetArrayFromImage(out_moving_label), tx, scale_factor)
+	# simg1 = sitk.Cast(sitk.RescaleIntensity(fixed), sitk.sitkUInt8)
+	# simg2 = sitk.Cast(sitk.RescaleIntensity(out), sitk.sitkUInt8)
+
+
 
 
 def channels_to_last(img: torch.Tensor):
--- perceiver/data/segmentation/miccai.py
+++ perceiver/data/segmentation/miccai.py
@@ -1,3 +1,4 @@
+import json
 import os
 import sys
 import copy
@@ -12,206 +13,253 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 from tqdm import tqdm
 
-from perceiver.data.segmentation.common import channels_to_last, SegmentationPreprocessor, lift_transform
+from perceiver.data.segmentation.common import channels_to_last, SegmentationPreprocessor, lift_transform, coregister_scan
 
-IMAGE_SIZE = (512, 512) # Adjustable meta parameter
-IMAGE_HEIGHT = 298 # Adjustable meta parameter
+IMAGE_SIZE = (1, 1, 1)
+SCAN_TO_COREGISTER_TO = None
 
 class MICCAIPreprocessor(SegmentationPreprocessor):
-    def __init__(self, normalize: bool = True, channels_last: bool = True):
-        super().__init__(miccai_transform(normalize, channels_last))
+	def __init__(self, normalize: bool = True, channels_last: bool = True):
+		super().__init__(miccai_transform(normalize, channels_last))
 
 
 class MICCAIDataset(Dataset) :
-    def __init__(self, data, transforms=None) :
-        self.data = data
-        self.transforms = transforms
+	def __init__(self, data, transforms=None) :
+		self.data = data
+		self.transforms = transforms
 
-    def set_transform(self, transforms) :
-        self.transforms = transforms
-        
-    def __getitem__(self, index):
-        sample = self.data[index]
-        if self.transforms:
-            sample = self.transforms(sample)
-        return sample
-    
-    def __len__(self) :
-        return len(self.data)
+	def set_transform(self, transforms) :
+		self.transforms = transforms
+		
+	def __getitem__(self, index):
+		sample = self.data[index]
+		if self.transforms:
+			sample = self.transforms(sample)
+		return sample
+	
+	def __len__(self) :
+		return len(self.data)
 
 
 class MICCAILoader() :
-    IMAGES_TR = "imagesTr"
-    LABELS_TR = "labelsTr"
-    
-    TRAIN_SIZE = 60
-    TEST_SIZE = 20
-    # VAL_SIZE = 20 # the rest
-    
-    BASIC_DATASET_ITEM = {'label' : None, 'image' : None}
+	IMAGES_TR = "imagesTr"
+	LABELS_TR = "labelsTr"
+	IMAGES_TR_PREPROCESSED = "imagesTr_preprocessed"
+	LABELS_TR_PREPROCESSED = "labelsTr_preprocessed"
+	
+	TRAIN_SIZE = 60
+	TEST_SIZE = 20
+	# VAL_SIZE = 20 # the rest
+	
+	BASIC_DATASET_ITEM = {'label' : None, 'image' : None, 'filename' : None}
 
-    LIMIT_SCAN_COUNT = 10
+	LIMIT_SCAN_COUNT = 15
 
-    DATA_DTYPE = np.float64
-    
-    def __init__(self, root) :
-        self.root = root
+	DATA_DTYPE = np.float64
+	
+	def __init__(self, root) :
+		self.root = root
 
-        if not os.path.exists(root) :
-            print("Cannot find dataset at specified root")
-            sys.exit(1)
+		if not os.path.exists(root) :
+			print("Cannot find dataset at specified root")
+			sys.exit(1)
 
-        self.images_dir = os.path.join(self.root, self.IMAGES_TR)
-        if not os.path.exists(self.images_dir) :
-            print("Cannot find imageTr inside dataset")
-            sys.exit(1)
+		self.images_dir = os.path.join(self.root, self.IMAGES_TR)
+		self.images_preprocessed_dir = os.path.join(self.root, self.IMAGES_TR_PREPROCESSED)
+		if not os.path.exists(self.images_dir) :
+			print("Cannot find imageTr inside dataset")
+			sys.exit(1)
 
-        self.labels_dir = os.path.join(self.root, self.LABELS_TR)
-        if not os.path.exists(self.labels_dir) :
-            print("Cannot find labelsTr inside dataset")
-            sys.exit(1)
+		self.labels_dir = os.path.join(self.root, self.LABELS_TR)
+		self.labels_preprocessed_dir = os.path.join(self.root, self.LABELS_TR_PREPROCESSED)
+		if not os.path.exists(self.labels_dir) :
+			print("Cannot find labelsTr inside dataset")
+			sys.exit(1)
 
-        self._no_files = min(len(os.listdir(self.labels_dir)), self.LIMIT_SCAN_COUNT)
+		self._no_files = min(len(os.listdir(self.labels_dir)), self.LIMIT_SCAN_COUNT)
 
-        self.data = []
-        self._load_data()
+		self.data = []
+		self._load_data()
 
-    def _load_data(self) :
-        for i in tqdm(range(self._no_files)) :
-            filename = os.listdir(self.images_dir)[i]
+	def _load_data(self) :
+		preprocess_data_dir_doesnt_exist = False
+		if not os.path.exists(self.images_preprocessed_dir) :
+			os.mkdir(self.images_preprocessed_dir)
+			preprocess_data_dir_doesnt_exist = True
+		if not os.path.exists(self.labels_preprocessed_dir) :
+			os.mkdir(self.labels_preprocessed_dir)
+			preprocess_data_dir_doesnt_exist = True
 
-            itk_img = sitk.ReadImage(os.path.join(self.images_dir, filename))
-            # cast array as torch cannot convert arrays of dtype=np.uint16 and transformations require floating point data
-            img = torch.from_numpy(sitk.GetArrayFromImage(itk_img).astype(self.DATA_DTYPE))
+		global IMAGE_SIZE, SCAN_TO_COREGISTER_TO
+		if preprocess_data_dir_doesnt_exist or (len(os.listdir(self.images_preprocessed_dir)) // 3 != self._no_files) :
+			for i in tqdm(range(self._no_files)) :
+				filename = os.listdir(self.images_dir)[i]
 
-            itk_img_seg = sitk.ReadImage(os.path.join(self.labels_dir, filename))
-            img_seg = torch.from_numpy(sitk.GetArrayFromImage(itk_img_seg).astype(self.DATA_DTYPE)) 
+				itk_img = sitk.ReadImage(os.path.join(self.images_dir, filename))
+				# cast array as torch cannot convert arrays of dtype=np.uint16 and transformations require floating point data
+				img = torch.from_numpy(sitk.GetArrayFromImage(itk_img).astype(self.DATA_DTYPE))
 
-            image_object = copy.copy(self.BASIC_DATASET_ITEM)
-            image_object['label'] = img_seg
-            image_object['image'] = img
-            self.data.append(image_object)
+				itk_img_seg = sitk.ReadImage(os.path.join(self.labels_dir, filename))
+				img_seg = torch.from_numpy(sitk.GetArrayFromImage(itk_img_seg).astype(self.DATA_DTYPE)) 
 
-    def get_split(self, split) :
-        _training_split = int(self._no_files * (100 / self.TRAIN_SIZE))
-        _test_split = int(self._no_files * (100 / self.TEST_SIZE))
-        _val_split = self._no_files - (_training_split + _test_split)
+				image_object = copy.copy(self.BASIC_DATASET_ITEM)
+				image_object['label'] = img_seg
+				image_object['image'] = img
+				image_object['filename'] = filename
+				self.data.append(image_object)
 
-        start_location = 0
-        split_size = self._no_files
-        if split == "train" :
-            start_location = 0
-            split_size = _training_split
-        elif split == "test" :
-            start_location = _training_split
-            split_size = _test_split
-        elif split == "val" :
-            start_location = _training_split + _test_split
-            split_size = _val_split
+				if (itk_img.GetWidth() * itk_img.GetHeight() * itk_img.GetDepth()) > (IMAGE_SIZE[0] * IMAGE_SIZE[1] * IMAGE_SIZE[2]) :
+					IMAGE_SIZE = (itk_img.GetWidth(), itk_img.GetHeight(), itk_img.GetDepth())
+					SCAN_TO_COREGISTER_TO = (filename, img.numpy())
 
-        return MICCAIDataset(self.data[start_location:split_size])
+			print("Coregistering scans to :", SCAN_TO_COREGISTER_TO[0], "of size", IMAGE_SIZE)
+
+			for i in tqdm(range(self._no_files)) :
+				image_object = self.data[i]
+				img_np = img_label_np = transformation = scaling = None
+
+				if image_object['filename'] != SCAN_TO_COREGISTER_TO[0] :
+					img_np, img_label_np, transformation, scaling = coregister_scan(
+						image_object['image'].numpy(),
+						image_object['label'].numpy(),
+						SCAN_TO_COREGISTER_TO[1]
+					)
+
+					image_object['image'] = torch.from_numpy(img_np)
+					image_object['label'] = torch.from_numpy(img_label_np)
+
+				self.data[i] = image_object
+				
+				sitk.WriteImage(sitk.GetImageFromArray(img_np), os.path.join(self.images_preprocessed_dir, image_object['filename']))
+				sitk.WriteImage(sitk.GetImageFromArray(img_label_np), os.path.join(self.labels_preprocessed_dir, image_object['filename']))
+				sitk.WriteTransform(transformation, os.path.join(self.images_preprocessed_dir, image_object['filename'].replace(".nii.gz", "_transformation.tfm")))
+				with open(os.path.join(self.images_preprocessed_dir, image_object['filename'].replace(".nii.gz", "_metadata.json")), "w") as f :
+					f.write(json.dumps({"scaling": scaling}))
+		
+		else  :
+			for i in tqdm(range(self._no_files)) :
+				filename = os.listdir(self.images_preprocessed_dir)[i]
+
+				itk_img = sitk.ReadImage(os.path.join(self.images_preprocessed_dir, filename))
+				# cast array as torch cannot convert arrays of dtype=np.uint16 and transformations require floating point data
+				img = torch.from_numpy(sitk.GetArrayFromImage(itk_img).astype(self.DATA_DTYPE))
+
+				itk_img_seg = sitk.ReadImage(os.path.join(self.labels_preprocessed_dir, filename))
+				img_seg = torch.from_numpy(sitk.GetArrayFromImage(itk_img_seg).astype(self.DATA_DTYPE)) 
+
+				image_object = copy.copy(self.BASIC_DATASET_ITEM)
+				image_object['label'] = img_seg
+				image_object['image'] = img
+				image_object['filename'] = filename
+				self.data.append(image_object)
+
+				if (img.shape[0] * img.shape[1] * img.shape[2]) > (IMAGE_SIZE[0] * IMAGE_SIZE[1] * IMAGE_SIZE[2]) :
+					IMAGE_SIZE = img.shape
+			
+	def get_split(self, split) :
+		_training_split = int(self._no_files * (100 / self.TRAIN_SIZE))
+		_test_split = int(self._no_files * (100 / self.TEST_SIZE))
+		_val_split = self._no_files - (_training_split + _test_split)
+
+		start_location = 0
+		split_size = self._no_files
+		if split == "train" :
+			start_location = 0
+			split_size = _training_split
+		elif split == "test" :
+			start_location = _training_split
+			split_size = _test_split
+		elif split == "val" :
+			start_location = _training_split + _test_split
+			split_size = _val_split
+
+		return MICCAIDataset(self.data[start_location:split_size])
 
 
 class MICCAIDataModule(pl.LightningDataModule):
-    def __init__(
-        self,
-        dataset_dir: str = "AMOS22",
-        normalize: bool = True,
-        channels_last: bool = True,
-        random_crop: Optional[int] = None,
-        batch_size: int = 64,
-        num_workers: int = 3,
-        pin_memory: bool = True,
-        shuffle: bool = True,
-        **kwargs
-    ):
-        super().__init__()
-        self.save_hyperparameters()
-        self.channels_last = channels_last
+	def __init__(
+		self,
+		dataset_dir: str = "AMOS22",
+		normalize: bool = True,
+		channels_last: bool = True,
+		random_crop: Optional[int] = None,
+		batch_size: int = 64,
+		num_workers: int = 3,
+		pin_memory: bool = True,
+		shuffle: bool = True,
+		**kwargs
+	):
+		super().__init__()
+		self.save_hyperparameters()
+		self.channels_last = channels_last
 
-        self.tf_train = miccai_transform(normalize, channels_last, random_crop=random_crop)
-        self.tf_valid = miccai_transform(normalize, channels_last, random_crop=None)
+		self.tf_train = miccai_transform(normalize, channels_last, random_crop=random_crop)
+		self.tf_valid = miccai_transform(normalize, channels_last, random_crop=None)
 
-        self.ds_train = None
-        self.ds_valid = None
-        
-        self.dataset_loader = None
+		self.ds_train = None
+		self.ds_valid = None
+		
+		self.dataset_loader = None
 
-    @property
-    def num_classes(self):
-        return 16
+	@property
+	def num_classes(self):
+		return 16
 
-    @property
-    def image_shape(self):
-        if self.hparams.channels_last:
-            return (IMAGE_SIZE[0], IMAGE_SIZE[1], IMAGE_HEIGHT)
-        else:
-            return (IMAGE_HEIGHT, IMAGE_SIZE[0], IMAGE_SIZE[1])
+	@property
+	def image_shape(self):
+		if self.hparams.channels_last:
+			return (IMAGE_SIZE[0], IMAGE_SIZE[1], IMAGE_SIZE[2])
+		else:
+			return (IMAGE_SIZE[2], IMAGE_SIZE[0], IMAGE_SIZE[1])
 
-    def load_dataset(self, split: Optional[str] = None):
-        if self.dataset_loader == None :
-            self.dataset_loader = MICCAILoader(self.hparams.dataset_dir)
-        
-        return self.dataset_loader.get_split(split)
+	def load_dataset(self, split: Optional[str] = None):
+		if self.dataset_loader == None :
+			self.dataset_loader = MICCAILoader(self.hparams.dataset_dir)
+		
+		return self.dataset_loader.get_split(split)
 
-    def prepare_data(self) -> None:
-        self.load_dataset()
+	def prepare_data(self) -> None:
+		self.load_dataset()
 
-    def setup(self, stage: Optional[str] = None) -> None:
-        self.ds_train = self.load_dataset(split="train")
-        self.ds_train.set_transform(lift_transform(self.tf_train))
+	def setup(self, stage: Optional[str] = None) -> None:
+		self.ds_train = self.load_dataset(split="train")
+		self.ds_train.set_transform(lift_transform(self.tf_train))
 
-        self.ds_valid = self.load_dataset(split="test")
-        self.ds_valid.set_transform(lift_transform(self.tf_valid))
+		self.ds_valid = self.load_dataset(split="test")
+		self.ds_valid.set_transform(lift_transform(self.tf_valid))
 
-        for i in range(len(self.ds_train)) :
-            print(self.ds_train[i]['image'].shape)
+		for i in range(len(self.ds_train)) :
+			print(self.ds_train[i]['image'].shape)
 
-        def showNii(img):
-            for i in range(img.shape[0]):
-                print(i)
-                plt.imshow(img[i, :, :], cmap='gray')
-                plt.show()
+	def train_dataloader(self):
+		return DataLoader(
+			self.ds_train,
+			shuffle=self.hparams.shuffle,
+			batch_size=self.hparams.batch_size,
+			num_workers=self.hparams.num_workers,
+			pin_memory=self.hparams.pin_memory,
+		)
 
-        # showNii(self.ds_train[0]['image'])
-
-    def train_dataloader(self):
-        return DataLoader(
-            self.ds_train,
-            shuffle=self.hparams.shuffle,
-            batch_size=self.hparams.batch_size,
-            num_workers=self.hparams.num_workers,
-            pin_memory=self.hparams.pin_memory,
-        )
-
-    def val_dataloader(self):
-        return DataLoader(
-            self.ds_valid,
-            shuffle=False,
-            batch_size=self.hparams.batch_size,
-            num_workers=self.hparams.num_workers,
-            pin_memory=self.hparams.pin_memory,
-        )
+	def val_dataloader(self):
+		return DataLoader(
+			self.ds_valid,
+			shuffle=False,
+			batch_size=self.hparams.batch_size,
+			num_workers=self.hparams.num_workers,
+			pin_memory=self.hparams.pin_memory,
+		)
 
 
 def miccai_transform(normalize: bool = True, channels_last: bool = True, random_crop: Optional[int] = None):
-    transform_list = []
+	transform_list = []
 
-    transform_list.append(transforms.Resize(IMAGE_SIZE))
-    
-    def stack_to_same_size(img: torch.Tensor) :
-        stack_images = torch.zeros((IMAGE_HEIGHT - img.shape[0], IMAGE_SIZE[0], IMAGE_SIZE[1]))
-        return torch.vstack([img, stack_images])
+	if random_crop is not None:
+		transform_list.append(transforms.RandomCrop(random_crop))
 
-    transform_list.append(stack_to_same_size)
+	if normalize:
+		transform_list.append(transforms.Normalize(mean=(0.5,), std=(0.5,)))
 
-    if random_crop is not None:
-        transform_list.append(transforms.RandomCrop(random_crop))
-
-    if normalize:
-        transform_list.append(transforms.Normalize(mean=(0.5,), std=(0.5,)))
-
-    if channels_last:
-        transform_list.append(channels_to_last)
-    
-    return transforms.Compose(transform_list)
+	if channels_last:
+		transform_list.append(channels_to_last)
+	
+	return transforms.Compose(transform_list)
