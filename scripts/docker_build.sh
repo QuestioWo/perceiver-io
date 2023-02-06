8ff871a72e2301029c792f04f4810f4eb7fa6abf
Jim Carty <cartyjim1@gmail.com>
2023-02-06 20:43:37 +0000

    Refining docker build script

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 model_path=$1
 coregistration_image_path=$2
+tagname=$3
 
 if [[ -z $model_path ]]; then
 	echo "Please supply a path to the model"
@@ -11,12 +14,16 @@ if [[ -z $coregistration_image_path ]]; then
 	exit 1
 fi
 
+if [[ -z $tagname ]]; then
+	tagname="ghcr.io/QuestioWo/miccai-perceiver-io:latest"
+fi
+
 echo "NOTE: This script must be ran with sudo when used on Ubuntu"
 
 cp $model_path ./best.ckpt
 cp $coregistration_image_path ./coregistration_image.nii.gz
 
-sudo docker build -t jimcarty-perceiver-io:latest \
+sudo docker build -t $tagname \
 	--build-arg MODEL_PATH=best.ckpt \
 	--build-arg COREGISTRATION_IMAGE_PATH=coregistration_image.nii.gz \
 	--build-arg RUN_INFERENCE_PATH=run_inference.py .
