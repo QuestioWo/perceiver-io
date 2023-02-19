da5fccfc8723d75635d9c207d065e4c5c73b1fd1
Jim Carty <cartyjim1@gmail.com>
2023-02-19 18:29:12 +0000

    Adding script to help with automating the network parameter search

---
+++ scripts/miccai/automated_optimise.sh
@@ -0,0 +1,16 @@
+python -m perceiver.scripts.segmentation.mapper fit \
+  --optimizer=AdamW \
+  --optimizer.lr=5e-4 \
+  --trainer.max_epochs=400 \
+  --trainer.logger=TensorBoardLogger \
+  --trainer.logger.save_dir=logs \
+  --trainer.logger.name=miccai_seg \
+  --data=MICCAIDataModule \
+  --data.batch_size=1 \
+  --trainer.log_every_n_steps=2 \
+  --data.num_workers=8 \
+  --trainer.check_val_every_n_epoch=1 \
+  --trainer.fast_dev_run=True \
+  --trainer.accelerator=gpu \
+  --trainer.devices=1
+#   --trainer.resume_from_checkpoint=logs/miccai_seg/version_5/checkpoints/last.ckpt
